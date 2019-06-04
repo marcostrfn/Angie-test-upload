09c1668ce33df42ac403f8d45a330f37bd80f09e
marcostrfn <marcos.trfn@gmail.com>
2019-06-04 22:56:26 +0200

    genero y edad en resumen de imagenes

--- test-upload/src/app/upload-detect/upload-detect.component.ts
+++ test-upload/src/app/upload-detect/upload-detect.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import * as faceapi from 'face-api.js';
 import { min } from 'rxjs/operators';
+import { SelectorFlags } from '@angular/compiler/src/core';
+import { FaceDetection } from 'face-api.js';
 
 
 @Component({
@@ -15,9 +17,9 @@ export class UploadDetectComponent implements OnInit {
   private imageInput: string;
   private imgData: string;
 
-  private canvasContainerResult:string;
-  private imagenesContainerResult:string;
-  
+  private canvasContainerResult: string;
+  private imagenesContainerResult: string;
+
 
   private MODEL_URL: string;
 
@@ -45,6 +47,8 @@ export class UploadDetectComponent implements OnInit {
       await faceapi.loadSsdMobilenetv1Model(self.MODEL_URL);
       await faceapi.loadFaceLandmarkModel(self.MODEL_URL);
       await faceapi.loadFaceRecognitionModel(self.MODEL_URL);
+      await faceapi.loadFaceExpressionModel(self.MODEL_URL);
+      await faceapi.loadAgeGenderModel(self.MODEL_URL);
       console.log('Fin carga de modelos FaceDetector');
     };
     run(this);
@@ -64,11 +68,92 @@ export class UploadDetectComponent implements OnInit {
   }
 
 
-  procesaPropagar(dataUrl) {
+  procesaPropagar(dataUrl: string): void {
     this.imgData = dataUrl;
     this.prepareImg2Canvas(dataUrl);
   }
 
+  private drawLineFace(f: any, ctx: CanvasRenderingContext2D): void {
+    // dibujo de lineas en rostros detectados
+    f.forEach(fd => {
+      ctx.beginPath();
+      ctx.moveTo(fd.detection.box.x, fd.detection.box.y);
+      ctx.lineTo(fd.detection.box.width + fd.detection.box.x, fd.detection.box.height + fd.detection.box.y);
+      ctx.stroke();
+
+      ctx.beginPath();
+      ctx.moveTo(fd.detection.box.x + fd.detection.box.width / 2, fd.detection.box.y);
+      ctx.lineTo(fd.detection.box.x + fd.detection.box.width / 2, fd.detection.box.y + fd.detection.box.height);
+      ctx.stroke();
+
+      ctx.beginPath();
+      ctx.moveTo(fd.detection.box.x, fd.detection.box.y + fd.detection.box.height / 2);
+      ctx.lineTo(fd.detection.box.x + fd.detection.box.width, fd.detection.box.y + fd.detection.box.height / 2);
+      ctx.stroke();
+    });
+  }
+
+
+  private createCanvasContainer(x: number) : CanvasRenderingContext2D {
+    const div = document.getElementById(this.canvasContainerResult);
+    const nodeCanvas = document.createElement('canvas');
+    const nombre = 'canvas' + x++;
+    console.log('nombre ', nombre)
+    nodeCanvas.setAttribute('id', nombre);
+    nodeCanvas.setAttribute('style', 'outline: black 3px solid;margin: 5px');
+    div.appendChild(nodeCanvas);
+
+    const elemento1 = nodeCanvas as HTMLCanvasElement
+    return elemento1.getContext('2d');
+  }
+
+  private DrawImageFromCanvasFace(fd: any, ctx: CanvasRenderingContext2D): void {
+
+    console.log(fd.age, fd.gender);
+    const age = fd.age;
+    const gender = fd.gender;
+    const nueva = new Image();
+    const self = this;
+    nueva.onload = (function (x: number, y: number, px: number, py: number) {
+      return function () {
+        ctx.canvas.width = px;
+        ctx.canvas.height = py;
+        ctx.drawImage(nueva, x, y, px, py, 0, 0, px, py);
+
+        // creamos una imagen nueva desde un canvas
+        const image = new Image();
+        image.setAttribute('class', 'img-thumbnail');
+        image.setAttribute('style', 'width: 150px;height: auto;');
+        image.src = ctx.canvas.toDataURL();
+
+        const divRowImg = document.createElement('div');
+        divRowImg.setAttribute('class', 'row');
+
+        const divColImg = document.createElement('div');
+        divColImg.setAttribute('class', 'col col-lg-2');
+
+        const divColDescImg = document.createElement('div');
+        divColDescImg.setAttribute('class', 'col col-lg-6');
+        
+        const pAge = document.createElement('h2');
+        pAge.innerHTML = age;
+
+        const pGender = document.createElement('h2');
+        pGender.innerHTML = gender;
+        
+        divColImg.appendChild(image);
+       
+        divColDescImg.appendChild(pGender);
+        divColDescImg.appendChild(pAge);
+        
+        divRowImg.appendChild(divColImg);
+        divRowImg.appendChild(divColDescImg);
+
+        document.getElementById(self.imagenesContainerResult).appendChild(divRowImg);
+      };
+    })(fd.detection.box.x, fd.detection.box.y, fd.detection.box.width, fd.detection.box.height);
+    nueva.src = this.imgData;
+  }
 
 
   private drawFace(): void {
@@ -87,63 +172,25 @@ export class UploadDetectComponent implements OnInit {
 
       console.log(input, mycanvas);
 
-      let fullFaceDescriptions = await faceapi.detectAllFaces(input).withFaceLandmarks().withFaceDescriptors().withFaceExpressions();
+      let fullFaceDescriptions = await faceapi.detectAllFaces(input)
+        .withFaceLandmarks()
+        .withFaceDescriptors()
+        .withFaceExpressions()
+        .withAgeAndGender();
+
       console.log(fullFaceDescriptions);
+      
 
-      // dibujo de lineas en rostros detectados
-      fullFaceDescriptions.forEach(fd => {
-        self.context.beginPath();
-        self.context.moveTo(fd.detection.box.x, fd.detection.box.y);
-        self.context.lineTo(fd.detection.box.width + fd.detection.box.x, fd.detection.box.height + fd.detection.box.y);
-        self.context.stroke();
-
-        self.context.beginPath();
-        self.context.moveTo(fd.detection.box.x + fd.detection.box.width / 2, fd.detection.box.y);
-        self.context.lineTo(fd.detection.box.x + fd.detection.box.width / 2, fd.detection.box.y + fd.detection.box.height);
-        self.context.stroke();
-
-        self.context.beginPath();
-        self.context.moveTo(fd.detection.box.x, fd.detection.box.y + fd.detection.box.height / 2);
-        self.context.lineTo(fd.detection.box.x + fd.detection.box.width, fd.detection.box.y + fd.detection.box.height / 2);
-        self.context.stroke();
-      });
-
+      self.drawLineFace(fullFaceDescriptions, self.context);
 
       let x = 0;
       fullFaceDescriptions.forEach(fd => {
-
-        const nueva = new Image();
-        const div = document.getElementById(self.canvasContainerResult);
-        const nodeCanvas = document.createElement('canvas');
-        const nombre = 'canvas' + x++;
-        console.log('nombre ', nombre)
-        nodeCanvas.setAttribute('id', nombre);
-        nodeCanvas.setAttribute('style', 'outline: black 3px solid;margin: 5px');
-        div.appendChild(nodeCanvas);
-
-        const elemento1 = document.getElementById(nombre) as HTMLCanvasElement
-        const context1 = elemento1.getContext('2d');
-
-        nueva.onload = (function (self: UploadDetectComponent, x: number, y: number, px: number, py: number) {
-          return function () {
-            context1.canvas.width = px;
-            context1.canvas.height = py;
-            context1.drawImage(nueva, x, y, px, py, 0, 0, px, py);
-
-            // creamos una imagen nueva desde un canvas
-            const image = new Image();
-            image.setAttribute('class', 'd-none img-fluid img-thumbnail');
-            image.src = context1.canvas.toDataURL();
-            document.getElementById(self.imagenesContainerResult).appendChild(image);
-
-          };
-        })(self, fd.detection.box.x, fd.detection.box.y, fd.detection.box.width, fd.detection.box.height);
-        nueva.src = self.imgData;
-
+        const context1 = self.createCanvasContainer(x++);
+        self.DrawImageFromCanvasFace(fd, context1);
       });
 
       // draw a textbox displaying the face expressions with minimum probability into the canvas
-      const displaySize = { width: self.context.canvas.width, height: self.context.canvas.height}
+      const displaySize = { width: self.context.canvas.width, height: self.context.canvas.height }
       const resizedResults = faceapi.resizeResults(fullFaceDescriptions, displaySize);
       const minProbability = 0.05;
       faceapi.draw.drawFaceExpressions(mycanvas, resizedResults, minProbability);
